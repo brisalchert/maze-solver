9e3bc844f161d2b930444852d8596a68aabec3ad
Brian Salchert <brisalchert@gmail.com>
2024-11-08 14:00:24 -0500

    Added node neighbor initialization

--- maze/Maze.py
+++ maze/Maze.py
@@ -18,6 +18,9 @@ class Maze:
         self.__initialize_nodes()
 
     def __initialize_nodes(self):
+        grid = {}
+
+        # Create nodes
         for x in range(self.length):
             for y in range(self.length):
                 # Create node
@@ -25,3 +28,66 @@ class Maze:
 
                 # Initialize adjacency list
                 self.graph[node] = []
+
+                # Add node to the temporary grid
+                grid[(x, y)] = node
+
+        # Initialize neighbors for each node
+        self.__initialize_neighbors(grid)
+
+    def __create_node(self, grid, x, y):
+        # Create the node
+        node = Node(x, y)
+
+        # Add the node to the grid
+        grid[(x, y)] = node
+
+        # Initialize an adjacency list to add node to the graph
+        self.graph[node] = []
+
+        # Recursively add nodes if there is room for nodes below or to the right
+        if x < self.length - 1:
+            self.__create_node(grid, x + 1, y)
+        if y < self.length - 1:
+            self.__create_node(grid, x, y + 1)
+
+        return grid
+
+    def __initialize_neighbors(self, grid):
+        for x in range(1, self.length):
+            col = x
+            row = 0
+
+            # Add neighbors downward until diagonal is reached
+            while row < col:
+                # Bi-directionally connect with left node
+                grid[(col, row)].add_neighbor(grid[(col - 1, row)])
+                grid[(col - 1, row)].add_neighbor(grid[(col, row)])
+
+                # Bi-directionally connect with upper node
+                if row > 0:
+                    grid[(col, row)].add_neighbor(grid[(col, row - 1)])
+                    grid[(col, row - 1)].add_neighbor(grid[(col, row)])
+
+                row += 1
+
+            # Add diagonal's neighbors
+            grid[(col, row)].add_neighbor(grid[(col - 1, row)])
+            grid[(col - 1, row)].add_neighbor(grid[(col, row)])
+            grid[(col, row)].add_neighbor(grid[(col, row - 1)])
+            grid[(col, row - 1)].add_neighbor(grid[(col, row)])
+
+            col -= 1
+
+            # Add neighbors to the left until edge is reached
+            while col >= 0:
+                # Bi-directionally connect with left node
+                if col > 0:
+                    grid[(col, row)].add_neighbor(grid[(col - 1, row)])
+                    grid[(col - 1, row)].add_neighbor(grid[(col, row)])
+
+                # Bi-directionally connect with upper node
+                grid[(col, row)].add_neighbor(grid[(col, row - 1)])
+                grid[(col, row - 1)].add_neighbor(grid[(col, row)])
+
+                col -= 1
--- maze/Node.py
+++ maze/Node.py
@@ -28,3 +28,25 @@ class Node:
         self.bottom_border = True
         self.left_border = True
         self.right_border = True
+
+        # Neighboring nodes
+        self.neighbors = []
+
+    def add_neighbor(self, neighbor):
+        """
+        Adds a neighbor node to this node's list of neighbors if it is not already present.
+        :param neighbor: the neighbor node
+        :return: True if the neighbor node was added, False otherwise
+        """
+        if not self.neighbors.__contains__(neighbor):
+            self.neighbors.append(neighbor)
+            return True
+
+        return False
+
+    def get_neighbors(self):
+        """
+        Accessor for a node's neighbors.
+        :return: The list of neighboring nodes
+        """
+        return self.neighbors
